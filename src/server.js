09fd85525ebc197672cf99e4bf4951e6f1312a0e
Renato Machado <81891866+Machadoo7@users.noreply.github.com>
2025-02-13 16:35:53 +0000

    feat: project finished

--- src/server.js
+++ src/server.js
@@ -1,24 +1,26 @@
 import http from "node:http"
+import { json } from "./middlewares/json.js"
+import { routes } from "./routes.js"
+import { extractQueryParams } from "./utils/extract-query-params.js"
 
-const users = []
-
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
- 
-  if(method === "GET" && url === "/users"){
-    return res
-      .setHeader("Content-type", "application/json")
-      .end(JSON.stringify(users))
-  }
 
-  if(method === "POST" && url === "/users"){
-    users.push({
-      id: 1,
-      name: "John Doe",
-      email: "johndoe@example.com"
-    })
+  await json(req, res)
 
-    return res.writeHead(201).end()
+  const route = routes.find(route => {
+    return route.method === method && route.path.test(url)
+  })
+
+  if(route){
+    const routeParams = req.url.match(route.path)
+
+    const { query, ...params } = routeParams.groups
+
+    req.params = params
+    req.query = query ? extractQueryParams(query) : {}
+
+    return route.handler(req, res)
   }
 
   return res.writeHead(404).end()
